fa7571ddd72dea24a5f08eaf4ab6f3de0edcfd5e
Anton Maksimov <anton.maximov@gmail.com>
2018-08-31 21:48:28 +0300

    Beacon UID int value

--- hh-location/models/beaconModel.go
+++ hh-location/models/beaconModel.go
@@ -2,7 +2,7 @@ package models
 
 type Beacon struct {
 	ID int `db:"id"`
-	Uid string `db:"uid"`
+	Uid int `db:"uid"`
 	Name string `db:"name"`
 	Correction int `db:"correction"`
 	PosX int `db:"pos_x"`
